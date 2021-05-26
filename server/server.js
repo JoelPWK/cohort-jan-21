cfb28a4c22d6dc94a00106684ab24e5117acbd7b
markh1089 <markhempel89@gmail.com>
2021-05-26 20:41:37 +0100

    Update server.js

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Server Started on port: ${PORT}`))
+
+
+
+
+
