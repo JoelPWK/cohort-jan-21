691d7405d109709c7463df13afd7e7a07955cb09
markh1089 <markhempel89@gmail.com>
2021-05-31 18:29:09 +0100

    addition of route to add a user

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,12 +2,15 @@ const express = require('express')
 const router = express.Router()
 const {User} = require ("../models/User")
 
-//
-
+//get a list of existing users
 router.route('/').get( async (req,res) => {
     User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/adduser').post((req,res) => {
+    const username
 })
 
 
