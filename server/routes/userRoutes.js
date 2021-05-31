f9ad4a46a8f7d98523dc799997d7613b90241142
markh1089 <markhempel89@gmail.com>
2021-05-31 18:25:13 +0100

    alternate connection

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const User = require ("../models/User")
+const {User} = require ("../models/User")
 
 //
+
+router.route('/').get( async (req,res) => {
+    User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
 
 module.exports = router;
 
--- server/server.js
+++ server/server.js
@@ -2,31 +2,39 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const mongoose = require ('mongoose');
-const bodyParser = require("body-parser")
 
+require("dotenv").config();
+
+//setting local port
+const port = process.env.PORT || 3001
+
+//middleware
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}))
 
 
-//mongoose connection
-mongoose.connect("mongodb+srv://dev:CQ2qAwKaBdxVC2BP@cluster0.hkypo.mongodb.net/auth?retryWrites=true&w=majority",
-{
+//obtaining routes
+const userRoutes = require('./routes/userRoutes')
+
+app.use('/register', userRoutes)
+
+//mongoatlas connection string
+const uri= process.env.CONNECTIONSTRING
+
+//mongodb connection
+
+mongoose.connect(uri, { 
     useUnifiedTopology: true, 
-    useNewUrlParser: true
-})
-//to get rid of depreciation warning within mongoose itself
-mongoose.set('useCreateIndex', true)
-
-//defining routes
-app.use("/", require("./routes/userRoutes"))
-
-app.get("/", function (req,res) {
-    res.send("express is working")
+    useNewUrlParser: true,
+    useCreateIndex: true
 })
 
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Mongodb connection complete')
+})
 
-const port = 3001
-app.listen(port, function(){
+app.listen(port, () =>{
     console.log(`you are running the express server on port: ${port}`)
 })
+
