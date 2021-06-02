1a798076f6cf1a4326c24df762803857811f52d7
Anna Georgiadou <a.d.georgiadou@gmail.com>
2021-06-02 15:48:46 +0100

    added a few comments

--- client/src/components/Carousel/CarouselBootstrap.js
+++ client/src/components/Carousel/CarouselBootstrap.js
@@ -1,4 +1,5 @@
 import React from 'react'
+// TODO::remove the bootstrap css from here and put it once in App.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Carousel.css'
 import Carousel from 'react-bootstrap/Carousel'
@@ -10,6 +11,7 @@ import image5 from '../../Images/food5.jpg'
 
 const CarouselBootstrap = () => {
     return (
+        //Placed the whole Carousel in a container to put some gradient background
         <div className='carouselContainer'>
             <h1><i>Create and browse delicious recipes</i></h1>
             <Carousel fade>
@@ -19,38 +21,33 @@ const CarouselBootstrap = () => {
                         alt="Pizza"
                     />
                 </Carousel.Item>
-
                 <Carousel.Item>
                     <img
                         src={image2}
                         alt="Sushi"
                     />
                 </Carousel.Item>
-
                 <Carousel.Item>
                     <img
                         src={image3}
                         alt="Salad"
                     />
                 </Carousel.Item>
-
                 <Carousel.Item>
                     <img
                         src={image4}
                         alt="Pasta"
                     />
                 </Carousel.Item>
-
                 <Carousel.Item>
                     <img
                         src={image5}
                         alt="Steak"
                     />
                 </Carousel.Item>
-
             </Carousel>
         </div>
     )
 }
 
-export default CarouselBootstrap
+export default CarouselBootstrap;
