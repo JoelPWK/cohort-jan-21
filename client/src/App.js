20232efb97858a8115ee065c0b7d52824566d52c
Anna Georgiadou <a.d.georgiadou@gmail.com>
2021-06-02 15:42:27 +0100

    bootstrap carousel resized and folder separated

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import Register from "./components/Register";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import CarouselBootstrap from './components/Carousel/CarouselBootstrap'
 
 function App() {
     return (
         <div className="App">
             <Navigation />
+            <CarouselBootstrap />
             <Router>
                 <Switch>
                     <Route exact path="/register">
---
+++ client/src/components/Carousel/CarouselBootstrap.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Carousel.css'
+import Carousel from 'react-bootstrap/Carousel'
+import image1 from '../../Images/food1.jpg'
+import image2 from '../../Images/food2.jpg'
+import image3 from '../../Images/food3.jpg'
+import image4 from '../../Images/food4.jpg'
+import image5 from '../../Images/food5.jpg'
+
+const CarouselBootstrap = () => {
+    return (
+        <div className='carouselContainer'>
+            <h1><i>Create and browse delicious recipes</i></h1>
+            <Carousel fade>
+                <Carousel.Item>
+                    <img
+                        src={image1}
+                        alt="Pizza"
+                    />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        src={image2}
+                        alt="Sushi"
+                    />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        src={image3}
+                        alt="Salad"
+                    />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        src={image4}
+                        alt="Pasta"
+                    />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        src={image5}
+                        alt="Steak"
+                    />
+                </Carousel.Item>
+
+            </Carousel>
+        </div>
+    )
+}
+
+export default CarouselBootstrap
