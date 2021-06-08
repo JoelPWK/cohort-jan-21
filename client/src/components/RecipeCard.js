4d2fe23004d743c84cad70c5b369e735aa62dbf9
markh1089 <markhempel89@gmail.com>
2021-06-08 20:43:41 +0100

    Update RecipeCard.js

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -5,23 +5,16 @@ const RecipeCard = (props) => {
   return (
     <div>
       <Card style={{ width: "18rem" }}>
+          <Card.Title>Recipe Name</Card.Title>
         <Card.Img variant="top" src="https://picsum.photos/200" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
           <Card.Text>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
             ipsum risus, suscipit ac venenatis sit amet, pellentesque eget
             felis. Proin augue nulla, fermentum sed egestas a, commodo non erat.
-            Sed id interdum velit, eget tempus nisi. Cras in sagittis justo. In
-            congue faucibus magna, aliquam ornare velit dignissim viverra.
-            Maecenas pharetra venenatis felis, rutrum sodales ligula ultricies
-            ac. In posuere vestibulum elit, et finibus nunc vestibulum ac.
-            Nullam eleifend tempus est in viverra. Donec ante arcu, efficitur
-            nec mi at, ullamcorper facilisis justo. Phasellus congue condimentum
-            nisl lobortis aliquet. Vivamus sit amet fringilla urna. Integer id
-            sagittis ex. Sed ullamcorper.
+            Sed id interdum velit, eget tempus nisi. Cras in sagittis justo. 
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary">See full Recipe</Button>
         </Card.Body>
       </Card>
     </div>
