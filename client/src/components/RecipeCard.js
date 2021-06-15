efb377106ee2c2e4d6b368d9e1a9e33763f2541c
markh1089 <markhempel89@gmail.com>
2021-06-15 19:15:25 +0100

    mapped fake data prior to backend link

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,22 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Card } from "react-bootstrap";
 
 const RecipeCard = (props) => {
+  const [recipeData, setRecipieData] = useState([
+    {
+      name:"chicken",
+      author: "Jim",
+      instructions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      photo: "https://picsum.photos/200",
+      likes: 5,
+    },
+    {
+      name:"chicken",
+      author: "Tim",
+      instructions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      photo: "https://picsum.photos/200",
+      likes: 50,
+    },
+    {
+      name:"chicken",
+      author: "Lynn",
+      instructions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      photo: "https://picsum.photos/200",
+      likes: 500,
+    },
+  ]);
+
   return (
     <div>
-      <Card style={{ width: "18rem" }}>
-          <Card.Title>Recipe Name</Card.Title>
-        <Card.Img variant="top" src="https://picsum.photos/200" />
-        <Card.Body>
-          <Card.Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            ipsum risus, suscipit ac venenatis sit amet, pellentesque eget
-            felis. Proin augue nulla, fermentum sed egestas a, commodo non erat.
-            Sed id interdum velit, eget tempus nisi. Cras in sagittis justo. 
-          </Card.Text>
-          <Button variant="primary">See full Recipe</Button>
-        </Card.Body>
-      </Card>
+      {recipeData.map((recipeData) => {
+        return (
+          <>
+          <div className="container-fluid d-flex justify-content-center">
+            <div className="row">
+              <div className="col-md-4">
+            <Card className='recipe cards' style={{ width: "18rem" }}>
+              <Card.Title>{recipeData.name}</Card.Title>
+              <Card.Subtitle>{recipeData.author}</Card.Subtitle>
+              <Card.Img variant="top" src={recipeData.photo} className="p-5" />
+              <Card.Body>
+                <Card.Text>
+                  {recipeData.instructions}
+                </Card.Text>
+                <Card.Text>{recipeData.likes}</Card.Text>
+                <Card.Link> See full recipe </Card.Link>
+                <Button variant="primary">Like</Button>
+              </Card.Body>
+            </Card>
+
+              </div>
+
+            </div>
+            </div>        
+          </>
+        );
+      })}
     </div>
   );
 };
