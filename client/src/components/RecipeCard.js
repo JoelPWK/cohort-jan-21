83332f31fb6eb20b651d685d0633deea8a6127e1
markh1089 <markhempel89@gmail.com>
2021-06-15 19:58:57 +0100

    working on styling and font awesome upvote down

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,57 +1,92 @@
 import React, { useState } from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, CardColumns } from "react-bootstrap";
+import { BiUpArrowCircle } from 'react-icons/bi'
+import { BiDownArrowCircle } from 'react-icons/bi'
 
 const RecipeCard = (props) => {
   const [recipeData, setRecipieData] = useState([
     {
-      name:"chicken",
+      name: "chicken",
       author: "Jim",
+      user: "https://i.pravatar.cc/100",
       instructions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       photo: "https://picsum.photos/200",
-      likes: 5,
+      likes: Math.floor(Math.random() * 100),
     },
     {
-      name:"chicken",
+      name: "Veg",
       author: "Tim",
+      user: "https://i.pravatar.cc/100",
       instructions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       photo: "https://picsum.photos/200",
-      likes: 50,
+      likes: Math.floor(Math.random() * 100),
     },
     {
-      name:"chicken",
+      name: "Pudding",
       author: "Lynn",
+      user: "https://i.pravatar.cc/100",
       instructions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       photo: "https://picsum.photos/200",
-      likes: 500,
+      likes: Math.floor(Math.random() * 100),
     },
   ]);
 
   return (
-    <div>
+    <div className='cardContainer'>
       {recipeData.map((recipeData) => {
         return (
           <>
-          <div className="container-fluid d-flex justify-content-center">
-            <div className="row">
-              <div className="col-md-4">
-            <Card className='recipe cards' style={{ width: "18rem" }}>
+            <div className="card">
+                <h1>{recipeData.name}</h1>
+              <img src={recipeData.photo} alt="Food Image" />
+              <div className="cardContent">
+                <div className="author">
+                  <img src={recipeData.user} />
+                  <div className="name">{recipeData.author}</div>
+                </div>
+                <p>{recipeData.instructions}</p>
+              </div>
+
+              <div className="footer">
+                
+                <div className="stats">
+                  <img
+                    className="upvote"
+                    src={BiUpArrowCircle}
+                    onClick={console.log('up voted')}
+                    alt="Upvote"
+                    />
+                  <img
+                    className="downvote"
+                    src={BiDownArrowCircle}
+                    onClick={console.log('down voted')}
+                    alt="down"
+                  />
+                  <div className="stat">{recipeData.likes}</div>
+                </div>
+              </div>
+            </div>
+
+            {/* <div style={{width:900, marginTop: 20}}>
+
+          <CardColumns>
+
+            <Card className="recipe cards" style={{ width: "18rem" }}>
               <Card.Title>{recipeData.name}</Card.Title>
               <Card.Subtitle>{recipeData.author}</Card.Subtitle>
-              <Card.Img variant="top" src={recipeData.photo} className="p-5" />
+              <img className='userimg' src={recipeData.user} alt='user image'></img>
+              <Card.Img variant="top" src={recipeData.photo} className="p-2" />
               <Card.Body>
-                <Card.Text>
-                  {recipeData.instructions}
-                </Card.Text>
-                <Card.Text>{recipeData.likes}</Card.Text>
+                <Card.Text>{recipeData.instructions}</Card.Text>
+                <Card.Text>Likes: {recipeData.likes}</Card.Text>
                 <Card.Link> See full recipe </Card.Link>
                 <Button variant="primary">Like</Button>
               </Card.Body>
             </Card>
 
-              </div>
+          </CardColumns>
 
-            </div>
-            </div>        
+          </div> */}
           </>
         );
       })}
