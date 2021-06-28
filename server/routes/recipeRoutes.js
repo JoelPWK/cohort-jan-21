98a60934e5a11140c03178a920f5cb23961d1842
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-06-28 21:44:55 +0100

    Remove accidental import

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { Recipe } = require("../models/Recipe");
 const { check, validationResult } = require("express-validator");
-const { request } = require("express");
 require("dotenv").config({ path: "./config/.env" });
 
 //get a list of existing recipes
